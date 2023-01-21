7977910a43587c3d08be8d78d914d994ef67516e
yongjae5717 <dydwo5717@naver.com>
2023-01-21 23:19:55 +0900

    resize 기능 개선 및 widget list에서 더블클릭시 파일 보여주기 기능 활성화

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 from PyQt5.QtWidgets import *
 from PyQt5 import uic, QtGui
-import cv2
 
 
 class MyGUI(QMainWindow):
@@ -21,7 +20,7 @@ class MyGUI(QMainWindow):
             pixmap = QtGui.QPixmap(self.current_file)
         except:
             pixmap = QtGui.QPixmap("default.png")
-        # pixmap = pixmap.scaled(self.width(), self.height())
+        pixmap = pixmap.scaled(self.width(), self.height())
         self.label.setPixmap(pixmap)
         self.label.resize(self.width(), self.height())
         self.file_list = None
@@ -36,6 +35,16 @@ class MyGUI(QMainWindow):
         self.pushButton.clicked.connect(self.next_image)
         # "View File List" 버튼
         self.pushButton_3.clicked.connect(self.show_list)
+        # widget list에서 더블클릭시 파일 보여주기 기능
+        self.listWidget.itemDoubleClicked.connect(self.click_image)
+
+    def click_image(self):
+        filename = str(self.file_list[self.listWidget.currentRow()])
+        if filename != "":
+            self.current_file = filename
+            pixmap = QtGui.QPixmap(self.current_file)
+            # pixmap = pixmap.scaled(self.width(), self.height())
+            self.label.setPixmap(pixmap)
 
     def open_image(self):
         self.listWidget.clear()
@@ -56,15 +65,6 @@ class MyGUI(QMainWindow):
         pixmap = QtGui.QPixmap(self.current_file)
         # pixmap = pixmap.scaled(self.width(), self.height())
         self.label.setPixmap(pixmap)
-
-    def crop(self):
-        options = QFileDialog.Options()
-        filename, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Image Files (*.png, *.jpg)", options=options)
-        if filename != "":
-            self.current_file = filename
-            pixmap = QtGui.QPixmap(self.current_file)
-            # pixmap = pixmap.scaled(self.width(), self.height())
-            self.label.setPixmap(pixmap)
 
     def next_image(self):
         if self.file_counter is not None and self.file_list:
@@ -100,29 +100,6 @@ def main():
     app = QApplication([])
     window = MyGUI()
     app.exec_()
-
-
-def cropping_img(img_path, col, row):
-    dest_dir = os.path.join('./', f"{col}_{row}_{col * row}cuts")
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
-
-    img = cv2.imread(img_path)
-    h, w, _ = img.shape
-
-    s_w = int(w / col)
-    s_h = int(h / row)
-    print(f"crop image size = {s_w}x{s_h}")
-
-    for n in range(1, (col * row) + 1):
-        x1 = int(((n - 1) % col) * s_w)
-        y1 = int(((n - 1) // col) * s_h)
-        x2 = x1 + s_w
-        y2 = y1 + s_h
-        print(f"{n} roi : ({x1}, {y1}), ({x2}, {y2})")
-
-        roi = img[y1:y2, x1:x2]
-        cv2.imwrite(os.path.join(dest_dir, f"{n}_cv.jpg"), roi)
 
 
 if __name__ == "__main__":
