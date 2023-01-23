6de2cac88b54cb1c4e3d96555eb1cd402b493df9
yongjae5717 <dydwo5717@naver.com>
2023-01-24 02:01:01 +0900

    이미지 크로핑 기능 추가

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+
+import cv2
 from PyQt5.QtWidgets import *
 from PyQt5 import uic, QtGui
 
@@ -10,10 +12,14 @@ class MyGUI(QMainWindow):
         uic.loadUi("imageviewer.ui", self)
         self.show()
         self.current_file = "default.png"
+        self.current_file_2 = "default.png"
         pixmap = QtGui.QPixmap(self.current_file)
+        pixmap_2 = QtGui.QPixmap(self.current_file_2)
         # pixmap = pixmap.scaled(self.width(), self.height())
         self.label.setPixmap(pixmap)
         self.label.setMinimumSize(1, 1)
+        self.label_3.setPixmap(pixmap_2)
+        self.label_3.setMinimumSize(1, 1)
 
     def resizeEvent(self, event):
         try:
@@ -37,6 +43,9 @@ class MyGUI(QMainWindow):
         self.pushButton_3.clicked.connect(self.show_list)
         # widget list에서 더블클릭시 파일 보여주기 기능
         self.listWidget.itemDoubleClicked.connect(self.click_image)
+        self.pushButton_5.clicked.connect(self.open_image_crop)
+        self.pushButton_4.clicked.connect(self.row_col_pressed)
+        self.pushButton_6.clicked.connect(self.return_pressed)
 
     def click_image(self):
         filename = str(self.file_list[self.listWidget.currentRow()])
@@ -56,6 +65,18 @@ class MyGUI(QMainWindow):
             pixmap = QtGui.QPixmap(self.current_file)
             # pixmap = pixmap.scaled(self.width(), self.height())
             self.label.setPixmap(pixmap)
+
+    def open_image_crop(self):
+        self.listWidget.clear()
+        self.file_list = list()
+        options = QFileDialog.Options()
+        filename, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Image Files (*.png, *.jpg)", options=options)
+        # print(filename)
+        if filename != "":
+            self.current_file2 = filename
+            pixmap_2 = QtGui.QPixmap(self.current_file2)
+            # pixmap = pixmap.scaled(self.width(), self.height())
+            self.label_3.setPixmap(pixmap_2)
 
     def open_directory(self):
         directory = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
@@ -94,6 +115,38 @@ class MyGUI(QMainWindow):
                 self.listWidget.insertItem(self.count, item)
                 self.count += 1
         self.label_2.setText(self.current_file)
+
+    def row_col_pressed(self):
+        self.row = self.lineEdit.text()
+        self.col = self.lineEdit_2.text()
+        self.label_4.setText(f"Row: {self.row}, Col: {self.col}")
+
+    def return_pressed(self):
+        row = int(self.row)
+        col = int(self.col)
+        img_path = self.current_file2
+        print(row, col, img_path)
+
+        dest_dir = os.path.join('./', f"{col}_{row}_{col * row}cuts")
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+
+        img = cv2.imread(img_path)
+        h, w, _ = img.shape
+
+        s_w = int(w / col)
+        s_h = int(h / row)
+        print(f"crop image size = {s_w}x{s_h}")
+
+        for n in range(1, (col * row) + 1):
+            x1 = int(((n - 1) % col) * s_w)
+            y1 = int(((n - 1) // col) * s_h)
+            x2 = x1 + s_w
+            y2 = y1 + s_h
+            print(f"{n} roi : ({x1}, {y1}), ({x2}, {y2})")
+
+            roi = img[y1:y2, x1:x2]
+            cv2.imwrite(os.path.join(dest_dir, f"{n}_cv.jpg"), roi)
 
 
 def main():
